aa44271af4db2c49c0cfc0a9a15f85a051c8ca5f
Farhan Ali Rahmoon <farhantcs13@gmail.com>
2021-04-19 20:35:13 -0700

    Add all sorting algorithms

---
+++ All Sorting Algorithms/src/Sorts.java
@@ -0,0 +1,210 @@
+public class Sorts {
+
+    public static void main(String[] args) {
+        // EMPTY
+    }
+
+    // Bubble Sort
+    public static void bubbleSort(int[] intArray) {
+        for (int lastUnsortedIndex = intArray.length - 1; lastUnsortedIndex > 0; lastUnsortedIndex--) {
+            for (int i = 0; i < lastUnsortedIndex; i++) {
+                if (intArray[i] > intArray[i + 1]) {
+                    swap(intArray, i, i + 1);
+                }
+            }
+        }
+
+        for (int i = 0; i < intArray.length; i++) {
+            System.out.println(intArray[i]);
+        }
+    }
+
+    public static void swap(int[] array, int i, int j) {
+        if (i == j) {
+            return;
+        }
+
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+    // Selection Sort
+    public static void selectionSort(int[] intArray) {
+        for (int lastUnsortedIndex = intArray.length - 1; lastUnsortedIndex > 0; lastUnsortedIndex--) {
+            int largest = 0;
+
+            for (int i = 1; i <= lastUnsortedIndex; i++) {
+                if (intArray[i] > intArray[largest]) {
+                    largest = i;
+                }
+            }
+
+            swap(intArray, largest, lastUnsortedIndex);
+        }
+
+        for (int i = 0; i < intArray.length; i++) {
+            System.out.println(intArray[i]);
+        }
+    }
+
+    // Insertion Sort
+    public static void insertionSort(int[] intArray) {
+        for (int firstUnsortedIndex = 1; firstUnsortedIndex < intArray.length; firstUnsortedIndex++) {
+            int newElement = intArray[firstUnsortedIndex];
+            int i;
+
+            for (i = firstUnsortedIndex; i > 0 && intArray[i - 1] > newElement; i--) {
+                intArray[i] = intArray[i - 1];
+            }
+
+            intArray[i] = newElement;
+        }
+
+        for (int i = 0; i < intArray.length; i++) {
+            System.out.println(intArray[i]);
+        }
+    }
+
+    // Shell Sort
+    public static void shellSort(int[] intArray) {
+        for (int gap = intArray.length / 2; gap > 0; gap /= 2) {
+            for (int i = gap; i < intArray.length; i++) {
+                int newElement = intArray[i];
+                int j = i;
+
+                while (j >= gap && intArray[j - gap] > newElement) {
+                    intArray[j] = intArray[j - gap];
+                    j -= gap;
+                }
+
+                intArray[j] = newElement;
+            }
+        }
+
+        for (int i = 0; i < intArray.length; i++) {
+            System.out.println(intArray[i]);
+        }
+    }
+
+    // Merge Sort
+    public static void mergeSort(int[] input, int start, int end) {
+        if (end - start < 2) {
+            return;
+        }
+
+        int mid = (start + end) / 2;
+        mergeSort(input, start, mid);
+        mergeSort(input, mid, end);
+        merge(input, start, mid, end);
+    }
+
+    public static void merge(int[] input, int start, int mid, int end) {
+        if (input[mid - 1] <= input[mid]) {
+            return;
+        }
+
+        int i = start;
+        int j = mid;
+        int tempIndex = 0;
+
+        int[] temp = new int[end - start];
+        while (i < mid && j < end) {
+            temp[tempIndex++] = input[i] <= input[j] ? input[i++] : input[j++];
+        }
+
+        System.arraycopy(input, i, input, start + tempIndex, mid - i);
+        System.arraycopy(temp, 0, input, start, tempIndex);
+    }
+
+    // Quick Sort
+    public static void quickSort(int[] input, int start, int end) {
+        if (end - start < 2) {
+            return;
+        }
+
+        int pivotIndex = partition(input, start, end);
+        quickSort(input, start, pivotIndex);
+
+        for (int i = 0; i < input.length; i++) {
+            System.out.println(input[i]);
+        }
+
+        quickSort(input, pivotIndex + 1, end);
+    }
+
+    public static int partition(int[] input, int start, int end) {
+        // This is using the first element as the pivot
+        int pivot = input[start];
+        int i = start;
+        int j = end;
+
+        while (i < j) {
+            // NOTE: empty loop body
+            while (i < j && input[--j] >= pivot);
+            if (i < j) {
+                input[i] = input[j];
+            }
+
+            // NOTE: empty loop body
+            while (i < j && input[++i] <= pivot);
+            if (i < j) {
+                input[j] = input[i];
+            }
+        }
+
+        input[j] = pivot;
+        return j;
+    }
+
+    // Counting Sort
+    public static void countingSort(int[] input, int min, int max) {
+        int[] countArray = new int[(max - min) + 1];
+
+        for (int i = 0; i < input.length; i++) {
+            countArray[input[i] - min]++;
+        }
+
+        int j = 0;
+        for (int i = min; i <= max; i++) {
+            while (countArray[i - min] > 0) {
+                input[j++] = i;
+                countArray[i - min]--;
+            }
+        }
+    }
+
+    // Radix Sort
+    public static void radixSort(int[] input, int radix, int width) {
+        for (int i = 0; i < width; i++) {
+            radixSingleSort(input, i, radix);
+        }
+    }
+
+    public static void radixSingleSort(int[] input, int position, int radix) {
+        int numItems = input.length;
+        int[] countArray = new int[radix];
+
+        for (int value: input) {
+            countArray[getDigit(position, value, radix)]++;
+        }
+        // Adjust the count array
+        for (int j = 1; j < radix; j++) {
+            countArray[j] += countArray[j - 1];
+        }
+
+        int[] temp = new int[numItems];
+        for (int tempIndex = numItems - 1; tempIndex >= 0; tempIndex--) {
+            temp[--countArray[getDigit(position, input[tempIndex], radix)]] =
+                    input[tempIndex];
+        }
+
+        for (int tempIndex = 0; tempIndex < numItems; tempIndex++) {
+            input[tempIndex] = temp[tempIndex];
+        }
+    }
+
+    public static int getDigit(int position, int value, int radix) {
+        return value / (int) Math.pow(radix, position) % radix;
+    }
+}
